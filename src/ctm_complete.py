9caacbfa92668cce372b5e806c3193e61cdbfee4
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2026-08-14 11:32:51 +1200

    Add WIP ctm 47-way class and methods

--- src/ctm_complete.py
+++ src/ctm_complete.py
@@ -2,20 +2,51 @@ from PIL import Image, ImageDraw, ImageOps
 # from pathlib import Path
 # import json
 
-class CTMComplete(ctm_dict: dict):
-    def __init__(self) -> None:
-         self.ctm_dict = ctm_dict
 
-del_dict = {
-        "n": (1, 0, 14, 0),
-        "ne": (15, 0, 15, 0),
-        "e": (15, 1, 15, 14),
-        "se": (15, 15, 15, 15),
-        "s": (1, 15, 14, 15),
-        "sw": (0, 15, 0, 15),
-        "w": (0, 1, 0, 14),
-        "nw": (0, 0, 0, 0)
-        }
+class CTMComplete:
+    def __init__(self, ctm_dict: dict, zones: dict) -> None:
+        """
+        Import zone coordinates and ctm definitions and set them to self variables.
+        """
+        self.ctm_dict = ctm_dict
+        self.zones = zones
+
+    def trim_zone(self, zone: str, sprite: Image.Image) -> Image.Image:
+        """
+        Overwrite a 16x image zone with transparent pixels based on a selected zone (cardinal direction) and return it.
+        """
+        sprite_draw = ImageDraw.Draw(sprite)
+        sprite_draw.rectangle(zones[zone], fill=(0, 0, 0, 0))
+        return sprite
+
+    def trim_ctm(self, num: int, sprite: Image.Image) -> Image.Image:
+        """
+        Overwrite multiple zones with transparent pixels based on ctm preset, looping through all zones in corresponding self.ctm_dict entry.
+        Contains exceptions for full borders (id=0) and no borders (id=26)
+        """
+        sprite_draw = ImageDraw.Draw(sprite)
+        if type(self.ctm_dict[num]) is int:
+            return sprite
+        elif type(self.ctm_dict[num]) is tuple:
+            sprite_centre = sprite.crop((1, 1, 15, 15))
+            sprite_full = ImageOps.expand(sprite_centre, border=1, fill=(0, 0, 0, 0))
+            return sprite_full
+        else:
+            for zone in self.ctm_dict[num]:
+                sprite_draw.rectangle(self.zones[zone], fill=(0, 0, 0, 0))
+            return sprite
+
+    def ctm_complete_gen(self, bg_path: str, overlay_path: str) -> list[Image.Image]:
+        bg = Image.open(bg_path)
+        overlay = Image.open(overlay_path)
+        ctm_images = []
+        for i in ctm_dict:
+            temp_bg = bg.copy()
+            temp_overlay = overlay.copy()
+            temp_overlay = self.trim_ctm(i, temp_overlay)
+            temp_bg.alpha_composite(temp_overlay)
+            ctm_images.append(temp_bg)
+        return ctm_images
 
 ctm_dict = {
     0: None,
