561204f1bbde05494e5ded3bdaf203dc29a2ac39
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2026-08-13 14:27:46 +1200

    Add ctm_meta_gen() to create .mcmeta and .properties metadata from template

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from PIL import Image, ImageDraw
 import os
+
+
+def ctm_meta_gen(name: str):
+    """
+    Returns .mcmeta and .properties strings for block name {name}
+    """
+
+    # mcmeta and properties template strings
+    properties_template = f"""
+    matchblocks={name}
+    method=ctm_compact
+    tiles=0-4
+    """
+    mcmeta_template = f"""
+    {"ctm":{"ctm_version": 1,
+            "type":"ctm",
+            "layer":"SOLID",
+            "textures":[
+                "minecraft:block/{name}_ctm"
+            ],
+            "extra":{"ignore_states":false,
+        	    "connect_inside":true
+            }
+        }
+    }
+    """
+
+    return properties_template, mcmeta_template
 def main():
     print("Hello from ore-gen!")
 
