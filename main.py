975f0a849a7270de3f3e373e93d1993789a64a00
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2026-08-14 13:15:01 +1200

    Add menu system for seperated codebase

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from PIL import Image
-from pathlib import Path
+# import builtin libraries
 import json
-from src.ctm_compact import ctm_compact_gen
+
+# import custom clases/functions
+from src.overlay import single_wrapper
+from src.ctm_compact import ctm_compact_wrapper
 
 
-def ctm_meta_gen(name: str) -> list[str]:
+def ctm_meta_gen(name: str, method: str) -> list[str]:
     """
     Returns .mcmeta and .properties strings for block name {name}
+    "method" must be either ctm or ctm_compact
     """
 
     # mcmeta and properties template strings
@@ -28,24 +31,27 @@ def ctm_meta_gen(name: str) -> list[str]:
     return [properties_template, json.dumps(mcmeta_template, indent=4)]
 
 
+def ctm_complete_wrapper():
+    pass
 
 
 def main():
-    bg_dir = Path("./sprites/stones/")
-    bgs = [f.stem for f in bg_dir.iterdir() if f.is_file()]
-    ore_dir = Path("./sprites/ores/")
-    patterns = [f.stem for f in ore_dir.iterdir() if f.is_file()]
-    for bg in bgs:
-        for pattern in patterns:
-            images = ctm_compact_gen(f"{bg_dir}/{bg}.png", f"{ore_dir}/{pattern}.png")
-            if bg == "stone":
-                Path(f"./output/{pattern}/").mkdir(exist_ok=True)
-                for i in range(5):
-                    images[i].save(f"./output/{pattern}/{i}.png")
-            else:
-                Path(f"./output/{bg}_{pattern}/").mkdir(exist_ok=True)
-                for i in range(5):
-                    images[i].save(f"./output/{bg}_{pattern}/{i}.png")
+    in_menu = True
+    while in_menu is True:
+        print(
+            "Select mode: \n1. Basic (full block outline, no ctm) \n2. Compact (ctm_compact 8way spec, 5 tiles per ore + metadata) \n3. Full (ctm 8-way spec, 47(6) tiles per ore + metadata) \n4. Exit"
+        )
+        mode = int(input(">>>"))
+        if mode == 1:
+            single_wrapper()
+        elif mode == 2:
+            ctm_compact_wrapper()
+        elif mode == 3:
+            ctm_complete_wrapper()
+        elif mode == 4:
+            in_menu = False
+        else:
+            print("  ^^^ Invalid option entered.")
 
 
 if __name__ == "__main__":
