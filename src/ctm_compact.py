d31a261f5bd702a9058fc1c24b09d94e45438755
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2026-08-14 13:10:21 +1200

    Add logging to ctm_compact_gen

--- src/ctm_compact.py
+++ src/ctm_compact.py
@@ -6,19 +6,26 @@ def ctm_compact_gen(base: str, overlay: str) -> list[Image.Image]:
     """
     Generates ctm tiles for ctm_compact (compact 8-way optifine spec) from 16x base and pattern images
     """
+    print("\n[ctm_compact_gen]: ctm_compact_gen initialised")
     # open images
     bg_raw = Image.open(base)
+    print(f"[ctm_compact_gen]: raw background image {base} loaded.")
     bg = bg_raw.convert("RGBA")
+    print(f"[ctm_compact_gen]: background image {base} converted to RGBA.")
+
     pattern = Image.open(overlay)
+    print(f"[ctm_compact_gen]: ore pattern image {pattern} load.")
 
     # generate 0.png (all sides)
     ctm_0 = bg.copy()
     ctm_0.alpha_composite(pattern)
+    print(f"[ctm_compact_gen]: ctm_0 texture generated for {pattern} on {base}.")
 
     # generate 1.png (no sides/borders)
     pattern_1 = pattern.crop((1, 1, 15, 15))
     ctm_1 = bg.copy()
     ctm_1.alpha_composite(pattern_1, (1, 1))
+    print(f"[ctm_compact_gen]: ctm_1 texture generated for {pattern} on {base}.")
 
     # generate 2.png (vertical borders only)
     pattern_2 = pattern.copy()
@@ -27,6 +34,7 @@ def ctm_compact_gen(base: str, overlay: str) -> list[Image.Image]:
     pattern_2_draw.rectangle((1, 15, 14, 15), fill=(0, 0, 0, 0))
     ctm_2 = bg.copy()
     ctm_2.alpha_composite(pattern_2)
+    print(f"[ctm_compact_gen]: ctm_2 texture generated for {pattern} on {base}.")
 
     # generate 3.png (horizontal borders only)
     pattern_3 = pattern.copy()
@@ -35,6 +43,7 @@ def ctm_compact_gen(base: str, overlay: str) -> list[Image.Image]:
     pattern_3_draw.rectangle((15, 1, 15, 14), fill=(0, 0, 0, 0))
     ctm_3 = bg.copy()
     ctm_3.alpha_composite(pattern_3)
+    print(f"[ctm_compact_gen]: ctm_3 texture generated for {pattern} on {base}.")
 
     # generate 4.png (corners only)
     pattern_4 = pattern.copy()
@@ -45,6 +54,7 @@ def ctm_compact_gen(base: str, overlay: str) -> list[Image.Image]:
     pattern_4_draw.rectangle((15, 1, 15, 14), fill=(0, 0, 0, 0))
     ctm_4 = bg.copy()
     ctm_4.alpha_composite(pattern_4)
+    print(f"[ctm_compact_gen]: ctm_4 texture generated for {pattern} on {base}.")
 
     # return image objects
     return [ctm_0, ctm_1, ctm_2, ctm_3, ctm_4]
